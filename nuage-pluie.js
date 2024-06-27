52953d65761eaae41edbed10f7d9cfe3e0e15c9c
ecliptien <keryann.vadell@gmail.com>
2024-06-27 15:23:52 +0200

    allez

--- nuage-pluie.js
+++ nuage-pluie.js
@@ -4,7 +4,7 @@ const weatherCard = document.querySelector('.weather-card');
 const weatherApiKey = "b38445e313331e1ccbe9d534b2ae461d";
 
 const cityName = document.getElementById('city');
-const cloud = document.getElementById('clouds')
+const cloud = document.getElementById('clouds.all')
 const rain = document.getElementById('rain.1h')
 
 
@@ -19,7 +19,8 @@ searchButton.addEventListener('click', () => {
       })
       .then(data => {
         cityName.innerText = data.name;
-        cloud.innerText = `${Math.round(data.main.cloud)}°`;
+        cloud.innerText = `${Math.round(data.main.cloud)}`;
+        rain.innerText = `${Math.round(data.main.rain)}`;
     });
     
   }});
