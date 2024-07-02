e3bea5e61a5f545f2a4cbc80c12eb1493f0d8c62
ecliptien <keryann.vadell@gmail.com>
2024-07-02 10:33:27 +0200

    .

--- nuagePluie.js
+++ nuagePluie.js
@@ -19,7 +19,7 @@ searchButton.addEventListener('click', () => {
       })
       .then(data => {
                 
-        clouds.innerText = `${Math.round(data.clouds.all)}`;
+        clouds.innerText = `${(data.clouds.all)}`;
         
         weather.innerText = data.weather[0].main;
         if (data.rain) {
